b47bee33602ac305fccd54930dfdfce1571be6e4
k <keith.ong5000@gmail.com>
2022-07-18 20:22:58 -0700

    style home page

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -2,23 +2,24 @@ import React from 'react';
 import './HomePage.css'
 import { BR } from 'country-flag-icons/react/3x2'
 import HomePageIcon from './HomePageIcon';
-import CreateButton from '../CreateButton/CreateButton';
+import {
+    useNavigate
+  } from "react-router-dom";
 const HomePage = () => {
+    const navigate = useNavigate()
+
     return (
         <div className='home-page'>
             {/* <div className='home-page-bar'></div> */}
-            <div className='home-overlay'></div>
+            {/* <div className='home-navbar'></div> */}
             <div className='stars'></div>
             <div className='shine-pattern'></div>
-            <div className='home-page-left'>   <BR title="Brazil" className="country-flag"/>  </div>
-            <div className='home-page-right'>
-            <button>HELLO WORLD</button>
-            <CreateButton setNewCardActive={() => {}} />
-
-                <div className='info-container'>
-                </div>
+            <div className='home-page-content'>
+                <HomePageIcon animation='pencil'/>
+                <div className='welcome-text'>Learn Portuguese with Cartão</div>
+                <div className='sub-heading'>Frequency dictionary guided flashcards teach you the most important words first</div>
+                <button className='start-button' onClick={() => navigate('/sign-in')}>Start Learning</button>
             </div>
-        
         </div>
     );
 };
